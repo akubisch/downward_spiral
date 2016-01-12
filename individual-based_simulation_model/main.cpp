75c16a90d754d5ed483b30727b73bd956b4c94f5
Alexander Kubisch <akubisch@posteo.de>
2016-01-12 15:12:20 +0100

    updated file preambles

--- individual-based_simulation_model/main.cpp
+++ individual-based_simulation_model/main.cpp
@@ -1,25 +1,19 @@
 /*
- * Simulation program for Kubisch et al. (2015) - 
- * "The downward spiral: Eco-evolutionary feedback loops lead to the
+ * Simulation program for Kubisch et al. (2016) - 
+ * "The downward spiral: eco-evolutionary feedback loops lead to the
  *  emergence of 'elastic' ranges."
  * 
  * main source file
- * 
- * Copyright (C) 2015 Dr. Alexander Kubisch
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Dr. Alexander Kubisch
+ * University of Hohenheim
+ * Institute for Landscape and Plant Ecology
+ * Stuttgart, Germany
+ * akubisch@posteo.de
+ * 2016
+ * © MIT License
  */
+
 
 #include <iostream>
 #include <cstdlib>							//standard C library
